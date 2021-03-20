4e6e2941ecf0311ea371afa2efc3f852bf6ed4c3
avrcoelho <andrevrcoelho@hotmail.com>
2021-03-20 19:14:17 -0300

    fix: description

--- react-native.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  dependency: {
-    platforms: {
-      android: null, // disable Android platform, other platforms will still autolink if provided
-      ios: null, // disable Android platform, other platforms will still autolink if provided
-    },
-  },
-};
